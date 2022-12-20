d8e454d022671fc4ced13d2658e6c10a32307195
Erica Camila <108944029+ericamila@users.noreply.github.com>
2022-12-20 16:42:29 -0400

    fim

--- app/src/main/java/com/curso/nova/ComidaActivity.java
+++ app/src/main/java/com/curso/nova/ComidaActivity.java
@@ -13,12 +13,22 @@ public class ComidaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comida);
-        setTitle("Consumo de comida:");
+        setTitle("Consumo de comida e bebidas:");
 
         TextView txvQtdComida = findViewById(R.id.tvComida);
-        txvQtdComida.setText(" Convidados: "+Convidado.getQtdConvidados()+" pessoas \n \n " +
-                " O total de alimentos sólidos necessários é: \n "+qtdSolido()+"kg \n \n" +
-                " O total de líquido é: \n"+qtdLiquido()+"L");
+        TextView txvQtdComidaTotal = findViewById(R.id.tvComidaTotal);
+        txvQtdComida.setText(" Convidados: "+(Convidado.getQtdConvidados())+" pessoas\n\n" +
+                " Consumo de suco: "+qtdLiquidoSuco()+"L\n" +
+                " Consumo de refrigetante: "+qtdLiquidoRefri()+"L\n" +
+                " Consumo de água: "+qtdLiquidoAgua()+"L\n" +
+                " Consumo de salgados: "+qtdSolidoSalgado()+"kg \n" +
+                " Consumo de doces: "+qtdSolidoDoce()+"kg \n" +
+                " Gramas do bolo: "+qtdSolidoBolo()+"kg \n" +
+                " Pratos: "+Convidado.getQtdConvidados()*2+" unidades \n" +
+                " Copos: "+Convidado.getQtdConvidados()*3+" unidades \n" +
+                " Talheres: "+(Convidado.getQtdConvidados()*2)+" unidades");
+        txvQtdComidaTotal.setText(" Total de alimentos sólidos necessários é:\n"+qtdSolido()+"kg\n\n" +
+                " Total de líquido é:\n"+qtdLiquido()+"L\n\n");
     }
 
     public String qtdLiquido(){
@@ -30,4 +40,33 @@ public class ComidaActivity extends AppCompatActivity {
         totalSolido = Convidado.getQtdConvidados()* 0.6f;
         return  String.format("%.3f",totalSolido);
     }
+
+    public String qtdLiquidoAgua(){
+        totalLiquido = Convidado.getQtdConvidados()* 1.5f;
+        return  String.format("%.2f",(totalLiquido/3)-(totalLiquido/3*0.5));
+    }
+    public String qtdLiquidoSuco(){
+        totalLiquido = Convidado.getQtdConvidados()* 1.5f;
+        return  String.format("%.2f",(totalLiquido/3));
+    }
+    public String qtdLiquidoRefri(){
+        totalLiquido = Convidado.getQtdConvidados()* 1.5f;
+        return  String.format("%.2f",(totalLiquido/3)+(totalLiquido/3*0.5));
+    }
+
+    public String qtdSolidoBolo(){
+        totalSolido = Convidado.getQtdConvidados()* 0.6f;
+        return  String.format("%.3f",(totalSolido/3)+(totalSolido/3)/3);
+    }
+
+    public String qtdSolidoSalgado(){
+        totalSolido = Convidado.getQtdConvidados()* 0.6f;
+        return  String.format("%.3f",(totalSolido/3));
+    }
+
+    public String qtdSolidoDoce(){
+        totalSolido = Convidado.getQtdConvidados()* 0.6f;
+        return  String.format("%.3f",(totalSolido/3)-(totalSolido/3)/3);
+    }
+
 }
